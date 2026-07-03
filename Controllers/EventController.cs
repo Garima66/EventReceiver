817309cd4de2d39c59bc10fab55e01ee4589bb3a
Garima <garima.gupta6688@gmail.com>
2026-07-03 13:04:47 +0530

    controller update Commit

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,16 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace EventReceiver.Controllers
 {
-
     [ApiController]
     [Route("api/events")]
     public class EventController : ControllerBase
     {
         [HttpPost]
-        public IActionResult Receive([FromBody] object data)
+        public IActionResult Receive([FromBody] JsonElement[] events)
         {
-            Console.WriteLine(data.ToString());
+            var eventData = events[0];
+
+            var eventType =
+                eventData.GetProperty("eventType").GetString();
+
+            if (eventType ==
+                "Microsoft.EventGrid.SubscriptionValidationEvent")
+            {
+                var validationCode =
+                    eventData.GetProperty("data")
+                             .GetProperty("validationCode")
+                             .GetString();
+
+                return Ok(new
+                {
+                    validationResponse = validationCode
+                });
+            }
+
+            Console.WriteLine(eventData.ToString());
+
             return Ok();
         }
     }
