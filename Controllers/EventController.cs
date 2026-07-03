36236eb0bd8d1d87ce4ed1369c3d815fec2324e2
Garima <garima.gupta6688@gmail.com>
2026-07-03 11:34:37 +0530

    initial commit

---
+++ Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventReceiver.Controllers
+{
+
+    [ApiController]
+    [Route("api/events")]
+    public class EventController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Receive([FromBody] object data)
+        {
+            Console.WriteLine(data.ToString());
+            return Ok();
+        }
+    }
+}
